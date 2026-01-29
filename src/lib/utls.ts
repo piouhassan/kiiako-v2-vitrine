a672ccb24e081f22a748c544c9f2c83bfac523ad
Hassan PIOU <piouhassan@gmail.com>
2026-01-29 12:25:13 +0000

    update design 28/01/2026 v2

--- src/lib/utls.ts
+++ src/lib/utls.ts
@@ -1,3 +1,7 @@
+/*
 export const REGISTER_URL_FREE =
     window.location.origin === "http://localhost:3000" ||
-    window.location.origin === "http://kiiako.localhost" ? "http://app.kiiako.localhost/register/free" : "https://app.kiiako.io/register/free";
+    window.location.origin === "http://kiiako.localhost" ? "http://app.kiiako.localhost/register/free" : "https://app.kiiako.io/register/free";*/
+
+
+export const REGISTER_URL_FREE = "https://app.kiiako.io/register/free";
